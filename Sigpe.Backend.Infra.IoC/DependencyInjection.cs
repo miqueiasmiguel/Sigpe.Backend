dbe1de92edfa71e684c3e948809c0dcdc748ca4b
Miquéias Miguel <miqueias.s.filho@gmail.com>
2024-03-15 10:42:32 -0300

    Adicionando configuração do banco de dados

---
+++ Sigpe.Backend.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sigpe.BackEnd.Infra.Data.Context;
+
+namespace Sigpe.Backend.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), b =>
+                b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+            return services;
+        }
+    }
+}
