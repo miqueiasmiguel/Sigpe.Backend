544eeb05a60fab9b5652a3079c4411076e2a7e25
Miquéias Miguel <miqueias.s.filho@gmail.com>
2024-03-17 13:07:35 -0300

    Criando dto e interface de serviço de Medicamento

---
+++ Sigpe.Backend.Application/Dtos/MedicamentoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Sigpe.Backend.Application.Dtos
+{
+    public class MedicamentoDto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ Sigpe.Backend.Application/Interfaces/IService.cs
@@ -0,0 +1,11 @@
+﻿namespace Sigpe.Backend.Application.Interfaces
+{
+    public interface IService<T>
+    {
+        public Task<T> CreateAsync(T dto);
+        public Task<T> UpdateAsync(T dto);
+        public Task<T> DeleteAsync(T dto);
+        public Task<IEnumerable<T>> GetAsync();
+        public Task<T?> GetByIdAsync(int id);
+    }
+}
