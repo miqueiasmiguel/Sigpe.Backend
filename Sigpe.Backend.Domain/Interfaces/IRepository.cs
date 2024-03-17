871859642698678b649d02df9fc94943c1ec18f7
Miquéias Miguel <miqueias.s.filho@gmail.com>
2024-03-17 10:49:57 -0300

    Criando interfaces de Repositório

---
+++ Sigpe.Backend.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Sigpe.Backend.Domain.Interfaces
+{
+    public interface IRepository<T>
+    {
+        public T Create(T entity);
+        public T Update(T entity);
+        public T Delete(T entity);
+        public List<T> Get();
+        public T GetById(int id);
+    }
+}
---
+++ Sigpe.Backend.Domain/Interfaces/IUsuario.cs
@@ -0,0 +1,8 @@
+﻿using Sigpe.Backend.Domain.Entities;
+
+namespace Sigpe.Backend.Domain.Interfaces
+{
+    public interface IUsuario : IRepository<Usuario>
+    {
+    }
+}
