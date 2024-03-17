8d4047d4f60ab251ad43fd1ea859e2473482fc5b
Miquéias Miguel <miqueias.s.filho@gmail.com>
2024-03-17 12:45:09 -0300

    Registrando injeção de dependência de repositórios

--- Sigpe.Backend.Infra.IoC/DependencyInjection.cs
+++ Sigpe.Backend.Infra.IoC/DependencyInjection.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Sigpe.Backend.Domain.Interfaces;
 using Sigpe.BackEnd.Infra.Data.Context;
+using Sigpe.BackEnd.Infra.Data.Repositories;
 
 namespace Sigpe.Backend.Infra.IoC
 {
@@ -12,6 +14,15 @@ namespace Sigpe.Backend.Infra.IoC
             services.AddDbContext<AppDbContext>(options =>
                 options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"), b =>
                 b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+            services.AddScoped<IAgendamentoRepository, AgendamentoRepository>();
+            services.AddScoped<IEspecialidadeRepository, EspecialidadeRepository>();
+            services.AddScoped<IMedicamentoRepository, MedicamentoRepository>();
+            services.AddScoped<IMedicoRepository, MedicoRepository>();
+            services.AddScoped<IPacienteRepository, PacienteRepository>();
+            services.AddScoped<IPlanoSaudeRepository, PlanoSaudeRepository>();
+            services.AddScoped<IPrescricaoRepository, PrescricaoRepository>();
+            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 
             return services;
         }
