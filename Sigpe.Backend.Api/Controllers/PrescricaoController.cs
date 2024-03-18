978f9008558ec63e6e79442a7701ef85738e5e27
Miquéias Miguel <miqueias.s.filho@gmail.com>
2024-03-18 07:36:12 -0300

    Criando controllers de prescrição e usuário

---
+++ Sigpe.Backend.Api/Controllers/PrescricaoController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sigpe.Backend.Application.Dtos;
+using Sigpe.Backend.Application.Interfaces;
+
+namespace Sigpe.Backend.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrescricaoController : ControllerBase
+    {
+        private readonly IPrescricaoService _prescricaoService;
+
+        public PrescricaoController(IPrescricaoService prescricaoService)
+        {
+            _prescricaoService = prescricaoService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var prescricoesDto = await _prescricaoService.GetAsync();
+
+            return Ok(prescricoesDto);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var prescricoesDto = await _prescricaoService.GetByIdAsync(id);
+
+            return Ok(prescricoesDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] PrescricaoDto dto)
+        {
+            var prescricaoDto = await _prescricaoService.CreateAsync(dto);
+
+            return Ok(prescricaoDto);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            await _prescricaoService.DeleteAsync(id);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsync([FromBody] PrescricaoDto dto)
+        {
+            var prescricaoDto = await _prescricaoService.UpdateAsync(dto);
+
+            return Ok(prescricaoDto);
+        }
+    }
+}
---
+++ Sigpe.Backend.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sigpe.Backend.Application.Dtos;
+using Sigpe.Backend.Application.Interfaces;
+
+namespace Sigpe.Backend.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuarioController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var usuariosDto = await _usuarioService.GetAsync();
+
+            return Ok(usuariosDto);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var usuariosDto = await _usuarioService.GetByIdAsync(id);
+
+            return Ok(usuariosDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] UsuarioDto dto)
+        {
+            var usuarioDto = await _usuarioService.CreateAsync(dto);
+
+            return Ok(usuarioDto);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            await _usuarioService.DeleteAsync(id);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsync([FromBody] UsuarioDto dto)
+        {
+            var usuarioDto = await _usuarioService.UpdateAsync(dto);
+
+            return Ok(usuarioDto);
+        }
+    }
+}
