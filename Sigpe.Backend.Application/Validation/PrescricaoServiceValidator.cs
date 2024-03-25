d8e0224763c549390c0d13f979c456036c04c2ae
Miquéias Miguel <miqueias.s.filho@gmail.com>
2024-03-25 20:32:52 -0300

    Melhorando validação de prescrições

--- Sigpe.Backend.Application/Validation/PrescricaoServiceValidator.cs
+++ Sigpe.Backend.Application/Validation/PrescricaoServiceValidator.cs
@@ -1,13 +1,28 @@
 ﻿using Sigpe.Backend.Application.Dtos;
 using Sigpe.Backend.Application.Interfaces.Validation;
+using Sigpe.Backend.Domain.Interfaces.Repositories;
 
 namespace Sigpe.Backend.Application.Validation
 {
     public class PrescricaoServiceValidator : IPrescricaoServiceValidator
     {
+        private readonly IMedicamentoRepository _medicamentoRepository;
+        private readonly IMedicoRepository _medicoRepository;
+        private readonly IPacienteRepository _pacienteRepository;
+
+        public PrescricaoServiceValidator(IMedicamentoRepository medicamentoRepository, IMedicoRepository medicoRepository, IPacienteRepository pacienteRepository)
+        {
+            _medicamentoRepository = medicamentoRepository;
+            _medicoRepository = medicoRepository;
+            _pacienteRepository = pacienteRepository;
+        }
+
         public async Task Validar(PrescricaoDto dto)
         {
             ValidarCamposObrigatorios(dto);
+            await ValidarMedicamento(dto);
+            await ValidarMedico(dto);
+            await ValidarPaciente(dto);
         }
 
         private void ValidarCamposObrigatorios(PrescricaoDto dto)
@@ -40,6 +55,41 @@ namespace Sigpe.Backend.Application.Validation
             if (!dto.MedicoId.HasValue)
             {
                 throw new ArgumentException($"{nameof(dto.MedicoId)} é obrigatório.");
+            }
+        }
+
+        private async Task ValidarMedicamento(PrescricaoDto dto)
+        {
+            var medicamento = await _medicamentoRepository.GetByIdAsync(dto.MedicamentoId.Value);
+
+            if (medicamento == null)
+            {
+                throw new Exception("Medicamento não encontrado");
+            }
+        }
+
+        private async Task ValidarMedico(PrescricaoDto dto)
+        {
+            var medico = await _medicoRepository.GetByIdAsync(dto.MedicoId.Value);
+
+            if (medico == null)
+            {
+                throw new Exception("Medico não encontrado");
+            }
+        }
+
+        private async Task ValidarPaciente(PrescricaoDto dto)
+        {
+            var paciente = await _pacienteRepository.GetByIdAsync(dto.PacienteId.Value);
+
+            if (paciente == null)
+            {
+                throw new Exception("Paciente não encontrado");
+            }
+
+            if (paciente.Alergias.Any(m => m.Id == dto.MedicamentoId))
+            {
+                throw new Exception("O paciente é alergico a este medicamento.");
             }
         }
     }
